aa594bc56eec5bb3e8cb65cebdce9dd7d6bac11b
qiyue <qiyue@wiseasy.com>
2017-12-07 19:30:20 +0800

    修改md

---
+++ app/src/main/java/com/wangpos/datastructure/java/AddTwoNumber.java
@@ -0,0 +1,66 @@
+package com.wangpos.datastructure.java;
+
+import com.wangpos.datastructure.R;
+import com.wangpos.datastructure.core.BaseActivity;
+
+/**
+ * Created by qiyue on 2017/12/7.
+ */
+
+public class AddTwoNumber extends BaseActivity {
+    @Override
+    protected void initData() {
+        setTitleText(getString(R.string.addtwonumber));
+    }
+
+    @Override
+    protected String getTextData() {
+        return null;
+    }
+
+    @Override
+    protected int getImageData() {
+        return 0;
+    }
+
+    @Override
+    protected String getResultData() {
+        return null;
+    }
+
+    @Override
+    protected String getTimeData() {
+        return null;
+    }
+
+    @Override
+    protected String getSpaceTimeData() {
+        return null;
+    }
+
+    @Override
+    protected String getWendingXingData() {
+        return null;
+    }
+
+    @Override
+    protected String getSummaryData() {
+        return null;
+    }
+
+
+
+    public ListNode addTwoNumber(ListNode l1,ListNode l2){
+
+
+        return null;
+
+    }
+
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+}
