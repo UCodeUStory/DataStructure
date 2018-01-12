4ab73792fccde4b19c4b417f82767c0b5420880c
qiyue <qiyue@wiseasy.com>
2018-01-12 21:40:19 +0800

    修改readme

--- app/src/main/java/com/wangpos/datastructure/MainActivity.java
+++ app/src/main/java/com/wangpos/datastructure/MainActivity.java
@@ -110,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         setWindowStatusBarColor(this,R.color.colorPrimary);
