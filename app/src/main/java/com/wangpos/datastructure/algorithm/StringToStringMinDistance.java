9639f3873ee7e59f47c48fb1c7e5a58e1c11f70d
qiyue <qiyue@wiseasy.com>
2020-05-17 16:35:30 +0800

    更新代码

--- app/src/main/java/com/wangpos/datastructure/algorithm/StringToStringMinDistance.java
+++ app/src/main/java/com/wangpos/datastructure/algorithm/StringToStringMinDistance.java
@@ -1,4 +1,49 @@
 package com.wangpos.datastructure.algorithm;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class StringToStringMinDistance {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public HashMap<String, ArrayList<String>> getNexts(List<String> words) {
+        Set<String> dict = new HashSet<>(words);
+
+        HashMap<String, ArrayList<String>> nexts = new HashMap<>();
+        for (int i = 0; i < words.size(); i++) {
+            nexts.put(words.get(i), new ArrayList<String>());
+        }
+        for (int i = 0; i < words.size(); i++) {
+            nexts.put(words.get(i), getNext(words.get(i), dict));
+        }
+        return nexts;
+    }
+
+    private ArrayList<String> getNext(String word, Set<String> dict) {
+        ArrayList<String> res = new ArrayList<>();
+        char[] chs = word.toCharArray();
+        for (char cur = 'a'; cur <= 'z'; cur++) {
+            for (int i = 0; i < chs.length; i++) {
+                if (chs[i] != cur) {
+                    char tmp = chs[i];
+
+                    chs[i] = cur; //替换
+                    if (dict.contains(String.valueOf(chs))) {
+                        res.add(String.valueOf(chs));
+                    }
+                    chs[i] = tmp; //替换回来
+                }
+            }
+        }
+    }
+
 }
