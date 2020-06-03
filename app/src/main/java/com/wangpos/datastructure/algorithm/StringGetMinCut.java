eae4caa7a34fb31f13df20dcad247f98ab5d02bd
qiyue <qiyue@wiseasy.com>
2020-06-03 10:00:55 +0800

    添加算法

---
+++ app/src/main/java/com/wangpos/datastructure/algorithm/StringGetMinCut.java
@@ -0,0 +1,19 @@
+package com.wangpos.datastructure.algorithm;
+
+public class StringGetMinCut {
+
+    public static void main(String[]args){
+
+        String originStr = "ACDCDCDAD";
+
+
+        //A 0
+        //AC 1
+        // ACD 2
+        // ACDC 1
+        // ACDCD 2
+        // ACDCDC 2
+
+
+    }
+}
